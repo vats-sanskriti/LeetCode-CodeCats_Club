f92bf911380c2c71cabcf356b261a96bf390bc38
Sanskriti <76396924+vats-sanskriti@users.noreply.github.com>
2024-03-06 13:34:41 +0530

    Time: 30 ms (97.17%), Space: 23.1 MB (99.63%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
+     bool ispalindrome(string& s){
+         int i = 0 ;
+         int e = s.size()-1;
+         while(i<e){
+         while(s[i]!= s[e])
+             return false;
+         
+         i++;
+         e--;}
+         return true;
+     }
+    
+    
     string firstPalindrome(vector<string>& words) {
-        for(auto i : words){
-            string s = i ;
-            reverse(i.begin() , i.end());
-            if(i == s ){
-                return s;
-            }
+        
+        for(string& st : words){
+            if(ispalindrome(st))
+                return st ;      
         }
+        
         return "";
+        
     }
 };
