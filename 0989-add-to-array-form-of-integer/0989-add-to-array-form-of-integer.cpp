d6b10b49fc553bc40019b233a18435a0665e23ab
Sanskriti <76396924+vats-sanskriti@users.noreply.github.com>
2024-03-11 18:33:20 +0530

    Time: 24 ms (25.06%), Space: 31.2 MB (26.01%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+    int n = num.size();
+        int i = n-1;
+        vector<int> ans;
+        while(i>=0 || k>0){
+            if(i>=0){
+               ans.push_back( (num[i]+k) %10);
+                k= (num[i]+k)/10 ;
+                i--;
+            }
+            
+            else{
+                ans.push_back(k%10);
+                k=k/10;
+                
+            }
+        }
+        reverse(ans.begin() , ans.end());
+        return ans;
+    
+    
+        
+        
+        
+    }
+};
